586fb9d1fbc022f024726f8e68c3b4b407801249
joota34256 <fuji_8761@yahoo.co.jp>
2021-05-10 15:43:00 +0900

    second commit

--- app/controllers/prototypes_controller.rb
+++ app/controllers/prototypes_controller.rb
@@ -1,6 +1,6 @@
 class PrototypesController < ApplicationController
   # before_action :move_to_index, expect: [:index, :show]
-  # before_action :authenticate_user!,except: [:index, :show]
+  before_action :authenticate_user!,except: [:index, :show]
 
   def index
     @prototypes = Prototype.all
@@ -34,9 +34,9 @@ class PrototypesController < ApplicationController
   end
 
   def update
-    prototype = Prototype.find(params[:id])
-    if prototype.update(prototype_params)
-      redirect_to root_path
+    @prototype = Prototype.find(params[:id])
+    if @prototype.update(prototype_params)
+      redirect_to prototype_path(@prototype.id)
     else
       render :edit
     end
